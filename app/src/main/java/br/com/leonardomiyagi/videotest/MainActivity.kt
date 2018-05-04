fde95823382e285b2dcb39015aebf244a2e6bbc4
Leonardo Miyagi <leonardomiyagi@gmail.com>
2018-05-04 17:25:40 -0400

    exoplayer

--- app/src/main/java/br/com/leonardomiyagi/videotest/MainActivity.kt
+++ app/src/main/java/br/com/leonardomiyagi/videotest/MainActivity.kt
@@ -1,12 +1,25 @@
 package br.com.leonardomiyagi.videotest
 
 import android.databinding.DataBindingUtil
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import br.com.leonardomiyagi.videotest.databinding.ActivityMainBinding
-import cn.jzvd.JZVideoPlayer
-import cn.jzvd.JZVideoPlayerStandard
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ext.okhttp.OkHttpDataSourceFactory
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.util.Util
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
+import io.reactivex.FlowableOnSubscribe
+import io.reactivex.rxkotlin.subscribeBy
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,20 +30,101 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.root.systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 
-        binding.videoPlayer.setUp("http://player.vimeo.com/external/250052982.sd.mp4?s=c74180b58ad296a03a6c9a44d128f8cb0b23021d&profile_id=165&oauth2_token_id=1029745847",
-                JZVideoPlayerStandard.SCREEN_WINDOW_FULLSCREEN, "Asdfdsaasdfdsa")
-        binding.videoPlayer.startWindowFullscreen()
-    }
+        val trackSelector = DefaultTrackSelector()
 
-    override fun onBackPressed() {
-        if (JZVideoPlayer.backPress()) {
-            return
-        }
-        super.onBackPressed()
-    }
+        val mediaSource = ExtractorMediaSource.Factory(DataSource.Factory {
+            OkHttpDataSourceFactory(OkHttpClient(), Util.getUserAgent(this, getString(R.string.app_name)), null).createDataSource()
+        }).createMediaSource(Uri.parse("http://player.vimeo.com/external/250052982.sd.mp4?s=c74180b58ad296a03a6c9a44d128f8cb0b23021d&profile_id=165&oauth2_token_id=1029745847"))
+        val mediaSource2 = ExtractorMediaSource.Factory(DataSource.Factory {
+            OkHttpDataSourceFactory(OkHttpClient(), Util.getUserAgent(this, getString(R.string.app_name)), null).createDataSource()
+        }).createMediaSource(Uri.parse("http://player.vimeo.com/external/250052982.sd.mp4?s=c74180b58ad296a03a6c9a44d128f8cb0b23021d&profile_id=165&oauth2_token_id=1029745847"))
 
-    override fun onPause() {
-        super.onPause()
-        JZVideoPlayer.releaseAllVideos()
+        val firstPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector)
+        val secondPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector)
+        firstPlayer.prepare(mediaSource)
+
+        binding.videoPlayer.player = firstPlayer
+        binding.videoPlayer.player.playWhenReady = true
+
+
+
+        Flowable.interval(1, TimeUnit.SECONDS)
+                .flatMap {
+                    Flowable.create(FlowableOnSubscribe<String> {
+                        it.onNext("Buffered ${binding.videoPlayer.player.bufferedPercentage} percent")
+                    }, BackpressureStrategy.LATEST)
+                }.subscribeBy(
+                        onNext = {
+                            println(it)
+                        },
+                        onError = {
+                            it.printStackTrace()
+                        },
+                        onComplete = {
+                            println("Complete")
+                        }
+                )
+
+        Flowable.interval(1, TimeUnit.SECONDS)
+                .flatMap {
+                    Flowable.create(FlowableOnSubscribe<String> {
+                        it.onNext("Content ${binding.videoPlayer.player.contentPosition}")
+                    }, BackpressureStrategy.LATEST)
+                }.subscribeBy(
+                        onNext = {
+                            println(it)
+                        },
+                        onError = {
+                            it.printStackTrace()
+                        },
+                        onComplete = {
+                            println("Complete")
+                        }
+                )
+
+        Flowable.interval(1, TimeUnit.SECONDS)
+                .flatMap {
+                    Flowable.create(FlowableOnSubscribe<String> {
+                        if (firstPlayer.playbackState == Player.STATE_READY) {
+                            it.onNext("Duration ${binding.videoPlayer.player.duration}")
+                            it.onComplete()
+                        }
+                    }, BackpressureStrategy.LATEST)
+                }.subscribeBy(
+                        onNext = {
+                            println(it)
+                        },
+                        onError = {
+                            it.printStackTrace()
+                        },
+                        onComplete = {
+                            println("Duration complete")
+                        }
+                )
+
+        /*
+        Flowable.interval(1, TimeUnit.SECONDS)
+                .flatMap {
+                    Flowable.create(FlowableOnSubscribe<String> {
+                        val asdf = firstPlayer.
+                        it.onNext("Buffered ${binding.videoPlayer.player.bufferedPercentage} percent")
+                    }, BackpressureStrategy.LATEST)
+                }.subscribeBy(
+                        onNext = {
+                            println(it)
+                        },
+                        onError = {
+                            it.printStackTrace()
+                        },
+                        onComplete = {
+                            println("Complete")
+                        }
+                )
+
+        player.prepare(videoSource);
+        player.seekTo(startTime);
+        player.setPlayWhenReady(true);
+
+         */
     }
 }
